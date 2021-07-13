1c51b4007dac6ff2b7125f05a6ee7b0567cfd753
Andrew Gallin <andrew.gallin@principles.com>
2021-07-13 16:26:30 -0400

    readability space

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export interface generateOptions {
   length?: number
   separator?: string
