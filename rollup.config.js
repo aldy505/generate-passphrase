8c757fe5f9364ac963e7fb10584901a6b9853b41
Reinaldy Rafli <aldy505@tutanota.com>
2021-07-14 12:09:34 +0700

    chore: bump dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ export default {
   external: ['crypto', 'fs', 'path', 'url'],
   plugins: [
     ts(),
-    // terser({format: {comments: 'all'}}),
+    terser({format: {comments: 'all'}}),
     copy({
       targets: [
         {src: 'src/words.txt', dest: 'dist'},
